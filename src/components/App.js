e64b38f612813c1808c3c886058f82749175b461
Tyler Cookson <tcookson0805@gmail.com>
2017-07-09 12:48:09 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+// import Config from '../config/config.js';
+
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello World!
+      </div>
+    );
+  }
+}
+
+export default App;
